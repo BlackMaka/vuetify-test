7ef0feeb3c7ff4b5168adb1ccf0b79a70a1eb727
suhan <completed0728@gmail.com>
2022-05-24 09:40:43 +0900

    confirmed 수정완료

--- src/views/authentication/SignUp.vue
+++ src/views/authentication/SignUp.vue
@@ -13,31 +13,33 @@
           <ValidationObserver>
             <v-card-text class="text-center px-12 py-16">
               <div class="text-h4 font-weight-black mb-10">회원가입</div>
-              <v-text-field
-                label="이메일"
-                clearable
-                prepend-icon="mdi-email"></v-text-field>
-              <ValidationProvider v-slot="{ errors }" vid="비밀번호">
+              <v-form>
                 <v-text-field
-                  v-model="pass"
-                  :error-message="{ errors }"
-                  label="비밀번호"
-                  prepend-icon="mdi-lock-outline"></v-text-field>
-              </ValidationProvider>
-              <ValidationProvider
-                v-slot="{ errors }"
-                name="비밀번호 확인"
-                :rules="{ confirmed: 'confirmation' }">
-                <v-text-field
-                  v-model="passConfirm"
-                  :error-message="{ errors }"
-                  label="비밀번호확인"
-                  prepend-icon="mdi-lock-outline"></v-text-field>
-              </ValidationProvider>
+                  label="이메일"
+                  clearable
+                  prepend-icon="mdi-email"></v-text-field>
+                <ValidationProvider v-slot="{ errors }" name="비밀버노">
+                  <v-text-field
+                    v-model="pass"
+                    :error-messages="errors"
+                    label="비밀번호"
+                    prepend-icon="mdi-lock-outline"></v-text-field>
+                </ValidationProvider>
+                <ValidationProvider
+                  v-slot="{ errors }"
+                  name="비밀번호 확인"
+                  :rules="{ confirmed: '비밀버노' }">
+                  <v-text-field
+                    v-model="passConfirm"
+                    :error-messages="errors"
+                    label="비밀번호확인"
+                    prepend-icon="mdi-lock-outline"></v-text-field>
+                </ValidationProvider>
 
-              <v-btn class="success lighten" large block rounded
-                >회원가입</v-btn
-              >
+                <v-btn class="success lighten" large block rounded
+                  >회원가입</v-btn
+                >
+              </v-form>
             </v-card-text>
           </ValidationObserver>
         </v-card>
